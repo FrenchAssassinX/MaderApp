0d943334cac3103d92154d5e989db90881484f8c
Robin COUTURIER <robin.couturier@viacesi.fr>
2019-11-06 14:12:03 +0100

    value and preppare to send

--- Assets/Scripts/NewProject.cs
+++ Assets/Scripts/NewProject.cs
@@ -2,10 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class NewProject : MonoBehaviour
 {
+    public GameObject CONST;
+    private string url;
+    private string URLCreateCustomer = "/v1/createcustomer";
+
     //project (CanvasLeft)
     public Input nameProject;
     public Input referenceProject;
@@ -18,6 +23,7 @@ public class NewProject : MonoBehaviour
     public Input roadNum;
     public Input road;
     public Input zipeCode;
+    public Input city;
     public Input roadExtra;
     public Input email;
     public Input phone;
@@ -28,6 +34,9 @@ public class NewProject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        url = CONST.GetComponent<CONST>().url;
+
         //it's not visible for now
         canvasNewClient.transform.gameObject.SetActive(false);
 
@@ -37,14 +46,48 @@ public class NewProject : MonoBehaviour
     void Update()
     {
         //Active canvasRight for add new customer
-        Button btn = ButtonCreateCustomer.GetComponent<Button>();
-        btn.onClick.AddListener(DisplayCreateNewClient);
+        Button btnCC = ButtonCreateCustomer.GetComponent<Button>();
+        btnCC.onClick.AddListener(DisplayCreateNewCustomer);
+
+        //send new project
+        Button btnCP = ButtonCreateProject.GetComponent<Button>();
+        btnCP.onClick.AddListener(SendCreateProjet);
+
+        //send new 
+        Button btnNC = ButtonCreateNewCustomer.GetComponent<Button>();
+        btnNC.onClick.AddListener(PostFormNewCustomer());
     }
 
-    void DisplayCreateNewClient()
+    void DisplayCreateNewCustomer()
     {
         //active CreateNewCient
         canvasNewClient.transform.gameObject.SetActive(true);
 
     }
+
+    void SendCreateProjet()
+    {
+
+    }
+
+    IEnumerator PostFormNewCustomer()
+    {
+        WWWForm form = new WWWForm();                   // New form for web request
+        form.AddField("name", name.text);
+        form.AddField("surname", surname.text);
+        form.AddField("roadNum", roadNum.int);
+        form.AddField("road", road.text);
+        form.AddField("zipeCode", zipeCode.int);
+        form.AddField("city", city.text);
+        form.AddField("roadExtra", roadExtra.text);
+        form.AddField("email", email.text);
+        form.AddField("phone", phone.int);
+        
+
+        using (UnityWebRequest request = UnityWebRequest.Post(url + URLCreateCustomer, form))
+        {
+
+        }
+    }
+
 }
