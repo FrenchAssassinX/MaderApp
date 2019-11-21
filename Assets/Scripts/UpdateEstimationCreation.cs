79b73a7e73bd9495e02d83ef4287bd5c7360d91c
Aurélien Thazet <thazet.aurelien@gmail.com>
2019-11-21 16:02:03 +0100

    Changing floor panels with buttons list: working

--- Assets/Scripts/UpdateEstimationCreation.cs
+++ Assets/Scripts/UpdateEstimationCreation.cs
@@ -10,28 +10,45 @@ public class UpdateEstimationCreation : MonoBehaviour
     //private string deleteProjectUrl = "v1/deleteproject";       // Specific route to delete one project
 
     public GameObject buttonFloorPrefab;                           // Prefab item to display all elements in project list
-    public GameObject gridList;                                 // Grid to insert project prefab items
+    public GameObject gridList;                                     // Grid to insert project prefab items
     private int floorCount = 0;                                     // Counter to set name of the floor
+
+    public GameObject panelFloorPrefab;
+    public GameObject panelsCanvas;
 
     void Start()
     {
         /* Instantiate automatic floors */
-        /* Groudn floor */
+        /* Ground floor */
         GameObject floor0 = Instantiate(buttonFloorPrefab, gridList.transform.position, Quaternion.identity);   // Creation of the prefab
-        floor0.name = "floor" + floorCount;
+        floor0.name = "Floor" + floorCount;
         GameObject text = GameObject.Find("FloorButtonText");                                                   // Find text in the prefab
         text.name = floor0.name + "Text";                                                                       // Change name of the texte
         text.GetComponent<UnityEngine.UI.Text>().text = "Rez-de-chaussée";                                      // Change text in the prefab
-        floor0.transform.SetParent(gridList.transform);                                                         // Set prefab as children of the gridList
-        floorCount++;                                                                                           // Increase counter of floors
+        floor0.transform.SetParent(gridList.transform);                                                         // Set prefab as child of the gridList
 
         /* Rooftop */
         GameObject rooftop = Instantiate(buttonFloorPrefab, gridList.transform.position, Quaternion.identity);  // Creation of the prefab
+        rooftop.name = "Rooftop";
         GameObject textRooftop = GameObject.Find("FloorButtonText");                                            // Find text in the prefab
         textRooftop.name = rooftop.name + "Text";                                                               // Change name of the texte
         textRooftop.GetComponent<UnityEngine.UI.Text>().text = "Toiture";                                       // Change text in the prefab
-        rooftop.transform.SetParent(gridList.transform);                                                        // Set prefab as children of the gridList
+        rooftop.transform.SetParent(gridList.transform);                                                        // Set prefab as child of the gridList
         /* Not increase floor counter for the rooftop: avoid counter error */
+
+        /* Adding panels for default floors */
+        /* Floor 0 */
+        GameObject panelFloor0 = Instantiate(panelFloorPrefab, panelsCanvas.transform.position, Quaternion.identity);   // Create new prefab
+        panelFloor0.name = "panelFloor" + floorCount;                                                                   // Change prefab name
+        panelFloor0.transform.SetParent(panelsCanvas.transform);                                                        // Set prefab as child of MiddleCanvas
+
+        /* Rooftop */
+        GameObject panelRooftop = Instantiate(panelFloorPrefab, panelsCanvas.transform.position, Quaternion.identity);   // Create new prefab
+        panelRooftop.name = "panelRooftop";                                                                   // Change prefab name
+        panelRooftop.transform.SetParent(panelsCanvas.transform);                                                        // Set prefab as child of MiddleCanvas
+
+        floorCount++;                                                                                           // Increase counter of floors
+
     }
 
     void Update()
@@ -41,12 +58,19 @@ public class UpdateEstimationCreation : MonoBehaviour
 
     public void AddFloor()
     {
+        /* Adding button in the list on down panel */
         GameObject newFloor = Instantiate(buttonFloorPrefab, gridList.transform.position, Quaternion.identity);  // Creation of the prefab
-        newFloor.name = "floor" + floorCount;                                                                    // Change prefab name
+        newFloor.name = "Floor" + floorCount;                                                                    // Change prefab name
         GameObject textNewFloor = GameObject.Find("FloorButtonText");                                            // Find text in the prefab
         textNewFloor.name = newFloor.name + "Text";                                                               // Change name of the texte
         textNewFloor.GetComponent<UnityEngine.UI.Text>().text = "Etage " + floorCount;                          // Change text in the prefab
         newFloor.transform.SetParent(gridList.transform);
+
+        /* Adding specific panel for the floor */
+        GameObject panelNewFloor = Instantiate(panelFloorPrefab, panelsCanvas.transform.position, Quaternion.identity);   // Create new prefab
+        panelNewFloor.name = "panelFloor" + floorCount;                                                                   // Change prefab name
+        panelNewFloor.transform.SetParent(panelsCanvas.transform);                                                        // Set prefab as child of MiddleCanvas
+
         floorCount++;                                                                                           // Increase counter of floors
 
     }
---
+++ Assets/Scripts/UpdateFloorButtonPrefab.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateFloorButtonPrefab : MonoBehaviour
+{
+    private GameObject middleCanvas;                                    // Parent canvas of the floor panels
+    public GameObject gridList;                                         // Parent of the buttons
+
+    private Button floorButton;                                         // Button on the list
+    private GameObject panelFloor;                                      // Panel for the concerned floor
+
+    public bool isSelected;                                             // Boolean for onClick function
+
+    void Start()
+    {
+        gridList = GameObject.Find("GridList");                         // Retrieve gridList on the scene
+
+        floorButton = GetComponent<Button>();                           // Retrieve button element on the scene
+        floorButton.onClick.AddListener(SelectItem);                    // Lauch item selected function
+        isSelected = false;                                             // Set to false on start
+
+        middleCanvas = GameObject.Find("MiddleCanvas");                 // Retrieve the parent canvas on the scene
+        panelFloor = GameObject.Find("panel" + floorButton.name);       // Retrieve panel element on the scene
+
+    }
+
+    void Update()
+    {
+        /* Launch function to display the concerned floor panel */
+        if (isSelected)
+        {
+            UnselectAllButtons();
+            DisplayAllFloorPanels();
+            DisplaySelectedFloorPanel();
+        }
+    }
+
+    /* Function for select item detection */
+    public void SelectItem()
+    {
+        isSelected = true;
+    }
+
+    /* Function to unselected all buttons of the gridList */
+    public void UnselectAllButtons()
+    {
+        foreach (Transform button in gridList.transform)
+        {
+            button.gameObject.GetComponent<UpdateFloorButtonPrefab>().isSelected = false;
+        }
+    }
+
+    /* Function to enable all the panels, in case of changing button */
+    public void DisplayAllFloorPanels()
+    {
+        /* Verify all the existing floor panels */
+        foreach (Transform panel in middleCanvas.transform)
+        {
+            panel.gameObject.SetActive(true);
+        }
+    }
+
+    /* Function to display only the panel of the current selected floor */
+    public void DisplaySelectedFloorPanel()
+    {
+        /* Verify all the existing floor panels */
+        foreach (Transform panel in middleCanvas.transform)
+        {
+            /* If the name of the panel is different from the currend selected floor: hide it */
+            if (panel.name != panelFloor.name)
+            {
+                panel.gameObject.SetActive(false);
+            }
+        }
+    }
+}
