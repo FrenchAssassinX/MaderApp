b7c3301f97b9d2372e46171a9711ef0822e92944
Robin COUTURIER <robin.couturier@viacesi.fr>
2019-11-22 16:19:38 +0100

    IHM , Dropdow and advancement bar for devis

--- Assets/Scripts/DevisPayment.cs
+++ Assets/Scripts/DevisPayment.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DevisPayment : MonoBehaviour
 {
+
+    public Dropdown stateDevis;
+    public Dropdown stateAdvancement;
+    public Image barStateAdvancement;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +20,27 @@ public class DevisPayment : MonoBehaviour
     {
         
     }
+
+    void StateDevis()
+    {
+        //Poster all devis state
+        List<string> dropdownStateDevis = new List<string>() {"Brouillon", "En attente", "Accepté", "Refusé", "En commande", "Transfert en facturation" /*temporaire*/};
+        stateDevis.AddOptions(dropdownStateDevis);
+    }
+
+    void StateAdvancement()
+    {
+        //Poster all devis advancement
+        List<string> dropdownStateAdvancement = new List<string>() {/*step*/};
+        stateAdvancement.AddOptions(dropdownStateAdvancement);
+    }
+
+    void BarStateAdvancement()
+    {
+
+    }
+
+
+
+
 }
