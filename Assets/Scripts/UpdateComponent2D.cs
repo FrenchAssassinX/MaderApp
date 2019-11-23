16ebc3fc0229e0caa93672fb90585dca18256b98
Aurelien Thazet <thazet.aurelien@gmail.com>
2019-11-23 12:25:29 +0100

    Can drag component on scene

--- Assets/Scripts/UpdateComponent2D.cs
+++ Assets/Scripts/UpdateComponent2D.cs
@@ -1,20 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class UpdateComponent2D : MonoBehaviour
+public class UpdateComponent2D : MonoBehaviour, IDragHandler, IEndDragHandler
 {
+    public bool isSelected;
+
+    private Button button;
+
+    private bool isPointerDown;
+    private float pointerDownTimer;
+    private float requiredHoldTime;
 
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        button = this.GetComponent<Button>();
+        button.onClick.AddListener(SelectComponent);
+
+        isSelected = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isSelected)
+        {
+            //Debug.Log("Component selected");
+
+        }
+    }
+
+    private void SelectComponent()
+    {
+        isSelected = true;
+    }
+
+    /*public void OnPointerDown(PointerEventData pEventData)
+    {
+        isPointerDown = true;
+        Debug.Log("PointerDown");
+    }
+
+    public void OnPointerUp(PointerEventData pEventData)
+    {
+        Reset();
+        Debug.Log("PointerUp");
+    }
+
+    private void Reset()
+    {
+        isPointerDown = false;
+        pointerDownTimer = 0;
+    }*/
+
+    public void OnDrag(PointerEventData pEventData)
+    {
+        //if (isSelected)
+        //{
+            GetComponent<RectTransform>().position = Input.mousePosition;
+        //}
+    }
+
+    public void OnEndDrag(PointerEventData pEventData)
+    {
+        GetComponent<RectTransform>().position = Input.mousePosition;
     }
 }
